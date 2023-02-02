19f3b949571f5e4595d3129d636e88d8d7624219
chekirko <victorche59@gmail.com>
2023-02-02 12:08:09 +0200

    merge two user endpoints into single

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,9 +1,11 @@
 const { User } = require('../../models');
+const { Pet } = require('../../models');
 
 const getCurrent = async (req, res) => {
     const { _id } = req.user;
     const user = await User.findById(_id);
-    res.json(user);
+    const pets = await Pet.find({ owner: _id });
+    res.json({user, pets});
 };
 
 module.exports = getCurrent;

6313c40761664caf909bf8da0b0fffbe0b7140c7
Olena-Zhyzhko <asil198609@gmail.com>
2023-02-11 17:44:47 +0200

    виправляє апдейт юзера

--- controllers/users/updateUserData.js
+++ controllers/users/updateUserData.js
@@ -4,14 +4,20 @@ const { RequestError, uploadToCloudinary } = require('../../helpers');
 const updateUserData = async (req, res) => {
     const { _id } = req.user;
     const updatedUser = req.body;
-    const locaFilePath = req.file.path;
-    const data = await uploadToCloudinary(locaFilePath);
-    const imageURL = data.url;
-    const result = await User.findByIdAndUpdate(_id, {
-        ...updatedUser,
-        avatarURL: imageURL,
+    let result;
+    if (!req.file) {
+        result = await User.findByIdAndUpdate(_id, { ...updatedUser },
+            { new: true });
+    } else {
+        const locaFilePath = req.file.path;
+        const data = await uploadToCloudinary(locaFilePath);
+        const imageURL = data.url;
+        result = await User.findByIdAndUpdate(_id, {
+            ...updatedUser,
+            avatarURL: imageURL,
         },
-        { new: true });
+            { new: true });
+    }
 
     if (!result) {
         throw RequestError(404, 'Not found');

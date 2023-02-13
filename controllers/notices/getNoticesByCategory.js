7eb94413dabfb66a9be69dacadbc383ea5b174a1
Olena-Zhyzhko <asil198609@gmail.com>
2023-02-13 16:25:32 +0200

    додає дату креейта

--- controllers/notices/getNoticesByCategory.js
+++ controllers/notices/getNoticesByCategory.js
@@ -5,7 +5,7 @@ const getNoticesByCategory = async (req, res) => {
     const { category } = req.params;
     const { page = 1, limit = 20 } = req.query;
     const skip = (page - 1) * limit;
-    const notices = await Notice.find({ category: category }, "-createdAt -updatedAt", { skip, limit: Number(limit) });
+    const notices = await Notice.find({ category: category }, "-updatedAt", { skip, limit: Number(limit) });
 
     if (!notices) {          
         throw RequestError(404, 'No any notices in this category');
